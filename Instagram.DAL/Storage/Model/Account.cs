fc9ad17f5809b8c5af448b26267a29080a0165b6
zikkryp <zikkryp@gmail.com>
2014-11-06 01:54:20 +0200

    added username, fullname, profilepicture properties

--- Instagram.DAL/Storage/Model/Account.cs
+++ Instagram.DAL/Storage/Model/Account.cs
@@ -21,11 +21,41 @@ namespace Instagram.DAL.Storage.Model
             this.IsActive = true;
         }
 
+        private string _username = string.Empty;
+        private string _profilePicture = string.Empty;
+        private string _fullName = string.Empty;
+
         [PrimaryKey]
         public int Id { get; private set; }
-        [MaxLength(100)]
         public string Token { get; private set; }
         public bool IsActive { get; set; }
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                if (_fullName == string.Empty)
+                {
+                    return this._username;
+                }
+                return _fullName;
+            }
+            set { _fullName = value; }
+        }
+
+        public string ProfilePicture
+        {
+            get { return _profilePicture; }
+            set { _profilePicture = value; }
+        }
+
+        public bool IsAutoSignIn { get; set; }
 
         public override string ToString()
         {

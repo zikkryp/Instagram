b244009982999e27f240ac43273219e6f456ffd2
zikkryp <zikkryp@gmail.com>
2014-11-06 01:55:59 +0200

    Bug fix and added authenticated user control

--- Instagram.UI/WelcomePage.xaml.cs
+++ Instagram.UI/WelcomePage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Instagram.DAL.Storage;
+﻿using Instagram.DAL.DataSource;
+using Instagram.DAL.Model;
+using Instagram.DAL.Storage;
+using Instagram.DAL.Storage.Model;
+using Instagram.UI.Common;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -20,20 +24,61 @@ namespace Instagram.UI
 {
     public sealed partial class WelcomePage : Page
     {
+
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
         public WelcomePage()
         {
             this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
 
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
             GetActiveAccount();
         }
+
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region Регистрация NavigationHelper
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private Account account;
 
         private async void GetActiveAccount()
         {
             try
             {
-                var account = await new Storage().GetActiveAccountAsync();
+                this.account = await new Storage().GetActiveAccountAsync();
 
-                if (account == null)
+                if (this.account == null)
                 {
                     //if (await new Authentication().AuthenticateAsync() != null)
                     //{
@@ -42,8 +87,15 @@ namespace Instagram.UI
 
                     return;
                 }
-                
-                this.Frame.Navigate(typeof(FeedPage));
+
+                gridViewItem.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+                await GetUserInfo();
+
+                if (this.account.IsAutoSignIn)
+                {
+                    this.Frame.Navigate(typeof(FeedPage));
+                }
             }
             catch (Exception e)
             {
@@ -60,8 +112,90 @@ namespace Instagram.UI
         {
             if (await new Authentication().AuthenticateAsync() != null)
             {
-                this.Frame.Navigate(typeof(FeedPage));
+                GetActiveAccount();
+                //this.Frame.Navigate(typeof(FeedPage));
             }
+        }
+
+        private async Task GetUserInfo()
+        {
+            ring.IsActive = true;
+            
+            userGrid.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+
+            try
+            {
+                var user = await UserSource.GetUserAsync();
+                this.account.Username = user.Username;
+                this.account.ProfilePicture = user.ProfilePicture;
+                this.account.FullName = user.FullName;
+                
+                this.defaultViewModel["User"] = this.account;
+
+                userGrid.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+                UpdateUserStorage();
+            }
+            finally
+            {
+                ring.IsActive = false;
+            }
+        }
+
+        private async void UpdateUserStorage()
+        {
+            Storage storage = new Storage();
+
+            //this.account = await storage.GetActiveAccountAsync();
+
+            //this.account.Username = user.Username;
+            //this.account.ProfilePicture = user.ProfilePicture;
+
+            await storage.UpdateItem<Account>(account);
+        }
+
+        private void SignIn_User(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(FeedPage));
+        }
+
+        private async void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Storage storage = new Storage();
+
+            try
+            {
+                if (await storage.DeleteItem<Account>(this.account.Id))
+                {
+                    this.gridViewItem.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                }
+            }
+            catch(Exception ex)
+            {
+                ShowDialog(ex.Message);
+            }
+        }
+
+        private async void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Storage storage = new Storage();
+
+            //this.account = await storage.GetActiveAccountAsync();
+
+            this.account.IsAutoSignIn = true;
+
+            await storage.UpdateItem<Account>(this.account);
+        }
+
+        private async void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Storage storage = new Storage();
+
+            //this.account = await storage.GetActiveAccountAsync();
+
+            this.account.IsAutoSignIn = false;
+
+            await storage.UpdateItem<Account>(this.account);
         }
     }
 }
